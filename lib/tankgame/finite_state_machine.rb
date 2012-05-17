3f3b708952386e5e4f58d43235ecb88813366319
Harry Garrood <hdgarrood@gmail.com>
2012-05-17 23:21:07 +0100

    Remove the idea of a stack of game states
    i probably wouldn't use it anyway

--- lib/tankgame/finite_state_machine.rb
+++ lib/tankgame/finite_state_machine.rb
@@ -3,18 +3,17 @@ require 'tankgame/game_states'
 
 module TankGame
   class FiniteStateMachine
-    attr_reader :window
+    attr_reader :current_state
+    attr_writer :next_state
 
     public
-    def initialize(window)
-      @window = window
-      @stack = [State::Game.new(window)]
+    def initialize
+      @current_state = State::Game.new
       @next_state = nil
     end
 
     def update
       change_state_if_needed
-      push_states
       current_state.update
     end
 
@@ -22,32 +21,14 @@ module TankGame
       current_state.draw
     end
 
-    def current_state
-      @stack.last
-    end
-
-    def set_next_state(state)
-      @next_state = state
-    end
-
-    def push_state(state)
-      @push_state = state
-    end
-
     private
     def change_state_if_needed
       unless @next_state.nil?
-        @stack.pop
-        @stack.push(@next_state.new(self.window))
+        @current_state = next_state.new
         @next_state = nil
       end
     end
 
-    def push_states
-      unless @push_state.nil?
-        @stack.push(@push_state.new(self.window))
-      end
-    end
   end
 end
 
