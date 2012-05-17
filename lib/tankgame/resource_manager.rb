ea148dda7880c0f72ef390b392798aa6870a876f
Harry Garrood <hdgarrood@gmail.com>
2012-05-17 23:45:35 +0100

    refactor ResourceManager to be used as an instance
    previously was planned to use it as a class

--- lib/tankgame/resource_manager.rb
+++ lib/tankgame/resource_manager.rb
@@ -1,17 +1,23 @@
 require 'gosu'
+require 'tankgame/game_objects'
 
 module TankGame
   class ResourceManager
-    attr_reader :sprites
+    RESOURCE_PATH = "resource/"
     IMAGE_PATH = "images/*.png"
 
-    def initialize(path = "resource")
-      @sprites = {} 
+    def load_sprites
+      sprites = {} 
 
-      Dir[File.join(path, IMAGE_PATH)].each do |file|
+      Dir[File.join(RESOURCE_PATH, IMAGE_PATH)].each do |file|
         sprite_name = File.basename(file, ".png")
-        @sprites[sprite_name] = Gosu::Image.new($window, file, false)
+        sprites[sprite_name] = Gosu::Image.new($window, file, false)
       end
+      return sprites
+    end
+
+    def sprites
+      @sprites ||= load_sprites
     end
   end
 end
