94be9b5cd2cdaaf0fd3b7b95844a16f72f3433c3
Harry Garrood <hdgarrood@gmail.com>
2012-05-19 00:03:19 +0100

    Adding a Player object. currently doesn't work...

--- lib/tankgame/game_objects.rb
+++ lib/tankgame/game_objects.rb
@@ -26,5 +26,20 @@ module TankGame
       @sprite = $window.resources.sprites['block']
     end
   end
+
+  class Player < GameObject
+    def initialize(x, y)
+      super(x, y)
+      @sprites = {
+        :tank => $window.resources.sprites['player'],
+        :barrel_l => $window.resources.sprites['barrel_left'],
+        :barrel_r => $window.resources.sprites['barrel_right']
+      }
+    end
+
+    def draw
+      @sprites['player'].draw(x, y, 0)
+    end
+  end
 end
 
