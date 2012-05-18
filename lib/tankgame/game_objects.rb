965d96b8f316564c6c19ec6095a256ac025c2e71
Harry Garrood <hdgarrood@gmail.com>
2012-05-19 00:34:32 +0100

    Adding movement -- exciting stuff!

--- lib/tankgame/game_objects.rb
+++ lib/tankgame/game_objects.rb
@@ -1,3 +1,5 @@
+require 'gosu'
+
 module TankGame
   class GameObject
     attr_reader :x, :y
@@ -35,10 +37,51 @@ module TankGame
         :barrel_l => $window.resources.sprites['barrel_left'],
         :barrel_r => $window.resources.sprites['barrel_right']
       }
+      @xspeed = @yspeed = 0
+      @status = {}
+    end
+
+    def handle_events
+      if $window.button_down? Gosu::KbD
+        @status[:motion] = :right
+      elsif $window.button_down? Gosu::KbA
+        @status[:motion] = :left
+      else
+        @status[:motion] = :none
+      end
+    end
+
+    def do_logic
+      case @status[:motion]
+      when :left
+        @xspeed -= acceleration
+      when :right
+        @xspeed += acceleration
+      end
+      @x += @xspeed
+      @y += @yspeed
+      if @xspeed.nonzero?
+        # friction reduces the player's speed
+        @xspeed > 0 ? @xspeed += friction : @xspeed -= friction
+      end
     end
 
     def draw
       @sprites[:tank].draw(x, y, 0)
+    end
+
+    private
+    # returns the current highest possible acceleration for the player
+    # (by means of the tank's engine) -- this should not be used for
+    # gravity, etc.
+    def acceleration
+      0.5
+    end
+
+    # the amount the player should slow down each step if he stops
+    # pressing buttons
+    def friction
+      0.7
     end
   end
 end
