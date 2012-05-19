36913653836d7a433109cc6dd57ff66b271fef87
Harry Garrood <hdgarrood@gmail.com>
2012-05-19 16:02:03 +0100

    make load_sprites a private method in ResourceManager

--- lib/tankgame/resource_manager.rb
+++ lib/tankgame/resource_manager.rb
@@ -3,21 +3,20 @@ require 'tankgame/game_objects'
 
 module TankGame
   class ResourceManager
-    RESOURCE_PATH = "resource/"
-    IMAGE_PATH = "images/*.png"
+    def sprites
+      @sprites ||= load_sprites
+    end
 
+    private
     def load_sprites
       sprites = {} 
 
-      Dir[File.join(RESOURCE_PATH, IMAGE_PATH)].each do |file|
+      Dir['resource/images/*.png'].each do |file|
         sprite_name = File.basename(file, ".png")
         sprites[sprite_name.to_sym] = Gosu::Image.new($window, file, false)
       end
-      return sprites
-    end
 
-    def sprites
-      @sprites ||= load_sprites
+      return sprites
     end
   end
 end
