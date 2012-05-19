3256eb005a688c5e58e705cd4f2caa080de2c8a6
Harry Garrood <hdgarrood@gmail.com>
2012-05-19 01:10:18 +0100

    make countdowns aware of whether they're started

--- lib/tankgame/countdown.rb
+++ lib/tankgame/countdown.rb
@@ -2,10 +2,16 @@ require 'time'
 require 'gosu'
 
 module TankGame
+  # TODO: check that Gosu::milliseconds won't overflow and bugger
+  # everything up.
   class Countdown
-    # starts the countdown timer with +time+ milliseconds on
-    # the clock.
+    def initialize
+      @started = false
+    end
+
+    # starts the countdown timer with +time+ milliseconds on the clock.
     def start(time)
+      @started = true
       @time = time
       @start_time = Gosu::milliseconds
       return self
@@ -13,12 +19,20 @@ module TankGame
 
     # returns time until end of countdown, in milliseconds
     def time_remaining
-      delta = Gosu::milliseconds - @start_time
-      [@time - delta, 0].max
+      if @started
+        delta = Gosu::milliseconds - @start_time
+        [@time - delta, 0].max
+      else
+        nil
+      end
     end
 
     def finished?
       time_remaining == 0
+    end
+
+    def started?
+      @started
     end
   end
 end
