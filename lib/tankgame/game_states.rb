dec42dd4ea1c2c519d1c08eb6abb212549b1991e
Harry Garrood <hdgarrood@gmail.com>
2012-05-19 16:18:51 +0100

    Adding a gradient background

--- lib/tankgame/game_states.rb
+++ lib/tankgame/game_states.rb
@@ -1,9 +1,12 @@
+require 'gosu'
 require 'tankgame/level_parser'
 
 module TankGame
   module State
     class Base
       def initialize
+        @bottom_colour = Gosu::Color.argb(0xff6b6b6b)
+        @top_colour = Gosu::Color.argb(0xfff0f0f0)
         @objects = []
       end
 
@@ -15,6 +18,7 @@ module TankGame
       end
 
       def draw
+        draw_background
         @objects.each do |o|
           o.draw
         end
@@ -23,10 +27,20 @@ module TankGame
                                                   $window.mouse_y, 0)
         end
       end
+
+      private
+      def draw_background
+        $window.draw_quad(0, 0, @top_colour,
+                          $window.width, 0, @top_colour,
+                          0, $window.height, @bottom_colour,
+                          $window.width, $window.height, @bottom_colour,
+                          0)
+      end
     end
 
     class Game < Base
       def initialize
+        super
         @objects = LevelParser.parse('one')
       end
 
